02e92489ebfa4652a4f3354c578f3a64c34ff64b
Junnan Zhu <junnan.zhu@nlpr.ia.ac.cn>
2020-12-18 16:32:25 +0800

    Update rouge.sh
    remove "python rouge.py"

--- resultofpaper/en2zh/rouge.sh
+++ resultofpaper/en2zh/rouge.sh
@@ -5,6 +5,5 @@ sed -i 's/<t> //g' $1
 python process.py $1
 ./chi_char_segment.pl -type plain < $1.new > $1.char
 python transform_to_enchar.py $1.char reference
-python rouge.py
 files2rouge $1.char.en reference.en > $2.rougescore
 rm $1.new $1.char $1.char.en reference.en
